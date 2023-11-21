f5c75205311f8218b6271c93a1ee3435b545c682
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-21 14:29:56 -0800

    Update run_DPS_opt.sh
    Add slurm changes for PSU cluster computer

--- Optimization/DPS/run_DPS_opt.sh
+++ Optimization/DPS/run_DPS_opt.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-#PBS -l walltime=1:00:00
-#PBS -l nodes=4:ppn=16
-#PBS -j oe
+#SBATCH -D /scratch/Lake_Problem_DPS/Optimization/DPS/
+#SBATCH -o /scratch/Lake_Problem_DPS/Optimization/DPS/output/job.%j.out   # Name of the output file (eg. myMPI.oJobID)
+#SBATCH -e /scratch/Lake_Problem_DPS/Optimization/DPS/output/error.%j.out
+#SBATCH -N 3            																# Total number of nodes to request (up to 120)
+#SBATCH --ntasks-per-node 20           													# Number of processors per node (up to 20)
+#SBATCH -p sla-prio           															# Queue name "parallel"
+#SBATCH -A azh       																# allocation name
+#SBATCH -t 1:00:00       											 					# Run time (hh:mm:ss) - up to 36 hours
+#SBATCH --mail-user=aas6791@psu.edu             										# address for email notification
+#SBATCH --mail-type=ALL                  												# email at Begin and End of job
 
-cd $PBS_O_WORKDIR
+cd $SLURM_SUBMIT_DIR
+
+module load openmpi boost/1.77.0
 
 # Your commands go here
 # arguments are <seed> <NFE>
 for i in {1..50}
 do
-  mpirun ./LakeDPSparallel $i 200000
+  srun -N3 -n20 ./LakeDPSparallel $i 200000
 done
