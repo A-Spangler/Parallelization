0b24cde8b425c4f40a4431fa670774c3fd16a58f
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-27 13:47:58 -0500

    Update hello_world_mpi.cpp
    update .cpp file to match the file

--- hello_world_mpi.cpp
+++ hello_world_mpi.cpp
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <mpi.h>
+#include "mpi.h"
 
-int main(int argc, char** argv){
-    int process_Rank, size_Of_Cluster;
+int main(int argc, char **argv){
+	int rank, size;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	printf( "Hello world from process %d of %d\n",rank,size);
+	MPI_Finalize();
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
-    MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
-
-    printf("Hello World from process %d of %d\n", process_Rank, size_Of_Cluster);
-    MPI_Finalize();
-
-    return 0;
+	return 0; 
 }
