26850cd53974e6a8f2d2994919cb4d92cad7b3ff
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-27 09:56:57 -0800

    add hello_world_mpi.sh file for testing
    this script should be deleted from the directory after tests are completed running. It should output a hello world script in parallel

---
+++ hello_world_mpi.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -D /storage/home/aas6791/work/Optimization/	# working directory
+#SBATCH -o /storage/home/aas6791/work/Optimization/mpi_test.%j.out   # Name of the output file
+#SBATCH -e /storage/home/aas6791/work/Optimization/mpi_error.%j.err # Name of the error file
+#SBATCH --account=azh5924_b
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --partition=sla-prio
+#SBATCH --time 0:01:00
+#SBATCH --mail-user=aas6791@psu.edu
+
+module load intel
+module load openmpi
+
+srun -N 1 ./hello_world_mpi.exe
+
+
+
+
+
+
+
+
+
+
+
+
