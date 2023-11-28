ca4208cdf986376c59f056f0f7df341084dab5e4
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-28 09:18:49 -0500

    change hello world to hello_world_mpi line 10

--- run_hello_world_mpi.sh
+++ run_hello_world_mpi.sh
@@ -7,6 +7,6 @@ srun hostname -f > $HOSTS
 sort $HOSTS | uniq -c | awk '{print $2 ":" $1}' >> $HOSTFILE
 
 echo "--------- The BEGINNING ---------" >> ./mpi.out
-srun -N1 -n4 -hostfile $HOSTFILE ./helloworld >> ./mpi.out
+srun -N1 -n4 -hostfile $HOSTFILE ./hello_world_mpi >> ./mpi.out
 
 rm $HOSTS $HOSTFILE
