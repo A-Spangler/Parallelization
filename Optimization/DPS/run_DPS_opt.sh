78455528302893c7802fd41f7a2f4f65974e6299
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-28 09:25:41 -0500

    Update run_DPS_opt.sh
    adjust time, partition, N3

--- Optimization/DPS/run_DPS_opt.sh
+++ Optimization/DPS/run_DPS_opt.sh
@@ -2,11 +2,11 @@
 #SBATCH -D /scratch/Lake_Problem_DPS/Optimization/DPS/
 #SBATCH -o /scratch/Lake_Problem_DPS/Optimization/DPS/output/job.%j.out   # Name of the output file (eg. myMPI.oJobID)
 #SBATCH -e /scratch/Lake_Problem_DPS/Optimization/DPS/output/error.%j.out
-#SBATCH -N 3            																# Total number of nodes to request (up to 120)
+#SBATCH -N3            																# Total number of nodes to request (up to 120)
 #SBATCH --ntasks-per-node 20           													# Number of processors per node (up to 20)
-#SBATCH -p sla-prio           															# Queue name "parallel"
+#SBATCH --partition=sla-prio           															# Queue name "parallel"
 #SBATCH --account=azh5924_b  																# allocation name
-#SBATCH -t 1:00:00       											 					# Run time (hh:mm:ss) - up to 36 hours
+#SBATCH --time=1:00:00       											 					# Run time (hh:mm:ss) - up to 36 hours
 #SBATCH --mail-user=aas6791@psu.edu             										# address for email notification
 #SBATCH --mail-type=ALL                  												# email at Begin and End of job
 
