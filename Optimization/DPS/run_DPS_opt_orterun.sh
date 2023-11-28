2488b2c903b4ef647d0543c66ddd08b5c1dc4b96
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-28 09:28:26 -0500

    Update run_DPS_opt_orterun.sh
    add code duplicate run_DPS_opt.sh

--- Optimization/DPS/run_DPS_opt_orterun.sh
+++ Optimization/DPS/run_DPS_opt_orterun.sh
@@ -1,1 +1,20 @@
-dd
+#!/bin/bash
+#SBATCH -D /scratch/Lake_Problem_DPS/Optimization/DPS/
+#SBATCH -o /scratch/Lake_Problem_DPS/Optimization/DPS/output/job.%j.out   # Name of the output file (eg. myMPI.oJobID)
+#SBATCH -e /scratch/Lake_Problem_DPS/Optimization/DPS/output/error.%j.out
+#SBATCH -N3            																# Total number of nodes to request (up to 120)
+#SBATCH --ntasks-per-node 20           													# Number of processors per node (up to 20)
+#SBATCH --partition=sla-prio           															# Queue name "parallel"
+#SBATCH --account=azh5924_b  																# allocation name
+#SBATCH --time=1:00:00       											 					# Run time (hh:mm:ss) - up to 36 hours
+#SBATCH --mail-user=aas6791@psu.edu             										# address for email notification
+#SBATCH --mail-type=ALL                  												# email at Begin and End of job
+
+module load openmpi boost/1.77.0
+
+# Your commands go here
+# arguments are <seed> <NFE>
+for i in {1..50}
+do
+  srun -N3 -n20 ./LakeDPSparallel $i 200000
+done
