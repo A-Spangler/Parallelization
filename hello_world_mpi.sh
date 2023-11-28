cd210e2956e65ae92b7d70edcc1bfe1653707f76
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-27 20:00:11 -0500

    Update output and error paths
    added LakeProblem/Lake_Problem_DPS/ directories

--- hello_world_mpi.sh
+++ hello_world_mpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -D /storage/home/aas6791/work/Optimization/	# working directory
-#SBATCH -o /storage/home/aas6791/work/Optimization/mpi_test.%j.out   # Name of the output file
-#SBATCH -e /storage/home/aas6791/work/Optimization/mpi_error.%j.err # Name of the error file
+#SBATCH -D /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/	# working directory
+#SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/mpi_test.%j.out   # Name of the output file
+#SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/mpi_error.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
