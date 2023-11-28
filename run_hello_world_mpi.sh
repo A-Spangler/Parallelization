627cc058420244e8ea831db52eed1e6ea4f7c0c4
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-27 21:50:28 -0500

    Update run_hello_world_mpi.sh

--- run_hello_world_mpi.sh
+++ run_hello_world_mpi.sh
@@ -7,6 +7,6 @@ srun hostname -f > $HOSTS
 sort $HOSTS | uniq -c | awk '{print $2 ":" $1}' >> $HOSTFILE
 
 echo "--------- The BEGINNING ---------" >> ./mpi.out
-mpiexec -hostfile $HOSTFILE ./helloworld >> ./mpi.out
+srun -N1 -n4 -hostfile $HOSTFILE ./helloworld >> ./mpi.out
 
 rm $HOSTS $HOSTFILE
