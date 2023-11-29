49210050cfe00c4da8d4dadf098bc84c7a2fb42f
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-29 11:26:02 -0500

    change second srun call to orterun

--- run_hello_world_mpi.sh
+++ run_hello_world_mpi.sh
@@ -7,6 +7,6 @@ srun -N3 -n10 hostname -f > $HOSTS
 sort $HOSTS | uniq -c | awk '{print $2 ":" $1}' >> $HOSTFILE
 
 echo "--------- The BEGINNING ---------" >> ./runmpi.out
-srun -N3 -n10 -hostfile $HOSTFILE ./hello_world_mpi >> ./runmpi.out
+orterun -hostfile $HOSTFILE ./hello_world_mpi >> ./runmpi.out
 
 rm $HOSTS $HOSTFILE
