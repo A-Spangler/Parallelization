50c45c4be16a79c041f0ff87b7276189b8347a0f
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-29 12:16:26 -0500

    changed Nodes and ntasks

--- hello_world_mpi.sh
+++ hello_world_mpi.sh
@@ -3,8 +3,8 @@
 #SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/mpi_test.%j.out   # Name of the output file
 #SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/mpi_error.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
-#SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --nodes=3
+#SBATCH --ntasks=10
 #SBATCH --partition=sla-prio
 #SBATCH --time 0:01:00
 #SBATCH --mail-user=aas6791@psu.edu
