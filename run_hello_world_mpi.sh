67870c5c0c5be7a76f5ed4dbc7edf3fe79d5b107
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-28 20:32:41 -0500

    deleted .exe from run --- begin line

--- run_hello_world_mpi.sh
+++ run_hello_world_mpi.sh
@@ -7,6 +7,6 @@ srun hostname -f > $HOSTS
 sort $HOSTS | uniq -c | awk '{print $2 ":" $1}' >> $HOSTFILE
 
 echo "--------- The BEGINNING ---------" >> ./runmpi.out
-srun -N1 -n4 -hostfile $HOSTFILE ./hello_world_mpi.exe >> ./runmpi.out
+srun -N1 -n4 -hostfile $HOSTFILE ./hello_world_mpi >> ./runmpi.out
 
 rm $HOSTS $HOSTFILE
