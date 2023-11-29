79473e08914a2e53c078a6530561f5570c6199d2
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-29 12:19:36 -0500

    added jobID to output

--- run_hello_world_mpi.sh
+++ run_hello_world_mpi.sh
@@ -6,7 +6,7 @@ HOSTFILE=.hostlist-job$SLURM_JOB_ID
 srun hostname -f > $HOSTS
 sort $HOSTS | uniq -c | awk '{print $2 ":" $1}' >> $HOSTFILE
 
-echo "--------- The BEGINNING ---------" >> ./runmpi.out
-orterun -hostfile $HOSTFILE ./hello_world_mpi >> ./runmpi.out
+echo "--------- The BEGINNING ---------" >> ./runmpi.%j.out
+orterun -hostfile $HOSTFILE ./hello_world_mpi >> ./runmpi.%j.out
 
 rm $HOSTS $HOSTFILE
