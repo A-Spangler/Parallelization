b57d35c596e2e6fc52c27265fb51b25e9ca613d3
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-30 13:43:50 -0500

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -3,8 +3,8 @@
 #SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/test2.%j.out   # Name of the output file
 #SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/error2.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
-#SBATCH --nodes=3
-#SBATCH --ntasks=3
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
 #SBATCH --partition=sla-prio
 #SBATCH --time 0:01:00
 #SBATCH --mail-user=aas6791@psu.edu
@@ -12,10 +12,6 @@
 module load parallel
 
 #job commands go below this line
-parallel echo ::: A B C :::
-  hostname
-  hostname
-  hostname &
 Parallel echo ::: A B C ::: 7 8 9 &
 Parallel echo ::: 77 66 55 ::: X Y Z
   
