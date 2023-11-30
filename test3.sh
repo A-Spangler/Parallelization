ee5f27af9a6537c5f2538b99dc3ae4fbe3053ab5
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-30 13:29:29 -0500

    Create test3.sh

---
+++ test3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -D /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/	# working directory
+#SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/test2.%j.out   # Name of the output file
+#SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/error2.%j.err # Name of the error file
+#SBATCH --account=azh5924_b
+#SBATCH --nodes=3
+#SBATCH --ntasks=3
+#SBATCH --partition=sla-prio
+#SBATCH --time 0:01:00
+#SBATCH --mail-user=aas6791@psu.edu
+
+#job commands go below this line
+srun -N1 -n1 test3script
