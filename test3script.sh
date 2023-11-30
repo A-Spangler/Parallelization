deda3878d96e8948fdae83e8112f34ca4e14e193
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-30 16:33:56 -0500

    Update test3script.sh

--- test3script.sh
+++ test3script.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#SBATCH -D /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/	# working directory
+#SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/test2.%j.out   # Name of the output file
+#SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/error2.%j.err # Name of the error file
+#SBATCH --account=azh5924_b
+#SBATCH --nodes=3
+#SBATCH --ntasks=3
+#SBATCH --partition=sla-prio
+#SBATCH --time 0:01:00
+#SBATCH --mail-user=aas6791@psu.edu
+
 srun parallel echo ::: A B C ::: 'Kitty' 2 3 &
 srun parallel echo ::: 77 'Tea' ::: 
   hostname &
