cd333a034a0f3b4d6b01157d121591e4d905cb66
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-30 17:07:13 -0500

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -3,7 +3,7 @@
 #SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/test2.%j.out   # Name of the output file
 #SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/error2.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
-#SBATCH --nodes=3
+#SBATCH --nodes=1
 #SBATCH --ntasks=3
 #SBATCH --partition=sla-prio
 #SBATCH --time 0:01:00
