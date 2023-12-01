48a8ca251c4540b11d918d3b2bef72fbfc53ce3a
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-30 19:57:30 -0500

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -4,7 +4,7 @@
 #SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/error2.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
 #SBATCH --nodes=3
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --partition=sla-prio
 #SBATCH --time 0:01:00
 #SBATCH --mail-user=aas6791@psu.edu
@@ -17,4 +17,4 @@ parallel echo ::: 77 'Tea' :::
   hostname &
 parallel echo :::
   hostname
-srun --no-kill --wait=0
+srun --no-kill --wait=0 $*
