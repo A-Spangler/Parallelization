c4d767512f1066e1a0cbaaf97c0835721f72301c
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-30 19:20:48 -0500

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -4,7 +4,7 @@
 #SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/error2.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
 #SBATCH --nodes=1
-#SBATCH --ntasks=3
+#SBATCH --ntasks=1
 #SBATCH --partition=sla-prio
 #SBATCH --time 0:01:00
 #SBATCH --mail-user=aas6791@psu.edu
@@ -12,4 +12,4 @@
 module load parallel 
 
 #job commands go below this line
-srun ./test3script.sh
+orterun ./test3script.sh
