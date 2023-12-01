fc259c86d87d1b0fd890c76420d23a84138e89f5
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-30 19:29:58 -0500

    Update run_IT_opt.sh

--- Optimization/Intertemporal/run_IT_opt.sh
+++ Optimization/Intertemporal/run_IT_opt.sh
@@ -2,8 +2,8 @@
 #SBATCH -D /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/Intertemporal
 #SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/Intertemporal/output/job.%j.out   # Name of the output file (eg. myMPI.oJobID)
 #SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/Intertemporal/output/error.%j.out
-#SBATCH -N3            																# Total number of nodes to request (up to 120)
-#SBATCH --ntasks-per-node 20           													# Number of processors per node (up to 20)
+#SBATCH --nodes=3           																# Total number of nodes to request (up to 120)
+#SBATCH --ntasks-per-node=20           													# Number of processors per node (up to 20)
 #SBATCH -partition=sla-prio           															# Queue name "parallel"
 #SBATCH --account=azh5924_b 																# allocation name
 #SBATCH -time=01:00:00       											 					# Run time (hh:mm:ss) - up to 36 hours
