6a8890798b718ccda2103ba8fa623ff58812d471
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-11-30 19:28:14 -0500

    change -ntasks to -ntasks-per-node

--- test3.sh
+++ test3.sh
@@ -4,7 +4,7 @@
 #SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/error2.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --partition=sla-prio
 #SBATCH --time 0:01:00
 #SBATCH --mail-user=aas6791@psu.edu
