85979fbfaac332f234f4cef902b0be0f44faaad2
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-12-04 14:55:44 -0500

    Update test4.sh

--- test4.sh
+++ test4.sh
@@ -3,7 +3,7 @@
 #SBATCH -o /storage/home/aas6791/scratch/Lake_Problem_DPS/test.%j.out   # Name of the output file
 #SBATCH -e /storage/home/aas6791/scratch/Lake_Problem_DPS/error.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
-#SBATCH --nodes=3
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=sla-prio
 #SBATCH --time 0:01:00
@@ -11,4 +11,4 @@
 
 
 #job commands go below this line 
-srun -N3 -n3 test4 #test 4 is a mpi script
+srun -N2 -ntasks-per-node=1 test4 #test 4 is a parallel script using 'parallel'
