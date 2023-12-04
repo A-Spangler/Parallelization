c0aab9e5a94ea232885e0f9a5e468a784391457d
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-12-04 14:32:09 -0500

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH -D /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/	# working directory
-#SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/test.%j.out   # Name of the output file
-#SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/error.%j.err # Name of the error file
+#SBATCH -D /storage/home/aas6791/scratch/aas6791/Lake_Problem_DPS	# working directory
+#SBATCH -o /storage/home/aas6791/scratch/aas6791/Lake_Problem_DPS/test.%j.out   # Name of the output file
+#SBATCH -e /storage/home/aas6791/scratch/aas6791/Lake_Problem_DPS/error.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=sla-prio
 #SBATCH --time 0:01:00
 #SBATCH --mail-user=aas6791@psu.edu
+
 
 module load parallel
 
