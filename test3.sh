f6a2122d53a70c103c6f399f4c0a3edb2b64851e
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-12-04 14:29:29 -0500

    change file path to scratch

--- test3.sh
+++ test3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -D /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/	# working directory
-#SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/test.%j.out   # Name of the output file
-#SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/error.%j.err # Name of the error file
+#SBATCH -D /storage/home/aas6791/scratch/aas6791/Lake_Problem_DPS	# working directory
+#SBATCH -o /storage/home/aas6791/scratch/aas6791/Lake_Problem_DPS/test.%j.out   # Name of the output file
+#SBATCH -e /storage/home/aas6791/scratch/aas6791/Lake_Problem_DPS/error.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
