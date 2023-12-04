d1aa7a544a60d557d1382593bebfb82a7a5f8b90
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-12-04 14:33:47 -0500

    Update test4.sh

--- test4.sh
+++ test4.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH -D /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/	# working directory
-#SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/test.%j.out   # Name of the output file
-#SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/error.%j.err # Name of the error file
+#SBATCH -D /storage/home/aas6791/scratch/aas6791/Lake_Problem_DPS	# working directory
+#SBATCH -o /storage/home/aas6791/scratch/aas6791/Lake_Problem_DPS/test.%j.out   # Name of the output file
+#SBATCH -e /storage/home/aas6791/scratch/aas6791/Lake_Problem_DPS/error.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
 #SBATCH --nodes=3
-#SBATCH --ntasks=3
+#SBATCH --ntasks-per-node=1
 #SBATCH --partition=sla-prio
 #SBATCH --time 0:01:00
 #SBATCH --mail-user=aas6791@psu.edu
+
 
 #job commands go below this line 
 srun -N3 -n3 test4.py #test 4 is a mpi script
