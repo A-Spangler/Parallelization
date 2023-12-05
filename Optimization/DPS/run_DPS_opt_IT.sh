5e3c5a69a5747425bf15721929c9082ab8842b16
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-12-05 09:08:25 -0500

    Run ups opt with changes suggested by IT

--- Optimization/DPS/run_DPS_opt_IT.sh
+++ Optimization/DPS/run_DPS_opt_IT.sh
@@ -2,19 +2,22 @@
 #SBATCH -D /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/DPS
 #SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/DPS/output/ortejob.%j.out   # Name of the output file (eg. myMPI.oJobID)
 #SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/DPS/output/orteerror.%j.out
-#SBATCH -N3            																# Total number of nodes to request (up to 120)
-#SBATCH --ntasks-per-node 20           													# Number of processors per node (up to 20)
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem-per-cpu=4gb
 #SBATCH --partition=sla-prio           															# Queue name "parallel"
 #SBATCH --account=azh5924_b  																# allocation name
 #SBATCH --time=1:00:00       											 					# Run time (hh:mm:ss) - up to 36 hours
 #SBATCH --mail-user=aas6791@psu.edu             										# address for email notification
 #SBATCH --mail-type=ALL                  												# email at Begin and End of job
 
-module load openmpi boost/1.77.0
+module load gcc
+module load openmpi
+module load boost/1.77.0
 
 # Your commands go here
 # arguments are <seed> <NFE>
 for i in {1..50}
 do
-	orterun -N3 -n20 ./LakeDPSparallel $i 200000
+srun -N3 -n20 ./LakeDPSparallel $i 200000
 done
