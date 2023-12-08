36d5ef1113d05aaf91d63cf4247843a129ef1fee
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-12-08 12:28:58 -0500

    Update test7.sh

--- test7.sh
+++ test7.sh
@@ -1,1 +1,21 @@
-b
+#!/bin/bash
+#SBATCH -D /storage/home/aas6791/scratch/Lake_Problem_DPS	# working directory
+#SBATCH -o /storage/home/aas6791/scratch/Lake_Problem_DPS/test.%j.out   # Name of the output file
+#SBATCH -e /storage/home/aas6791/scratch/Lake_Problem_DPS/error.%j.err # Name of the error file
+#SBATCH --account=azh5924_b
+#SBATCH --partition=sla-prio
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=4:00:00
+#SBATCH --mail-user=aas6791@psu.edu
+#SBATCH --export=ALL
+#SBATCH --exclusive=mcs
+#SBATCH --mem=0
+
+module load parallel
+
+srun="srun -n 1 -c $SLURM_CPUS_ON_NODE "
+parallel="parallel --max-procs $SLURM_CPUS_ON_NODE --delay 0.2" 
+$srun $parallel "time" ::: {0..10} 
+$srun $parallel "time" ::: A B C 
