164a02c6fa5ec009f3a51504e88dc5df6bf9135c
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-12-08 13:48:43 -0500

    Update test5.sh

--- test5.sh
+++ test5.sh
@@ -4,7 +4,7 @@
 #SBATCH -e /storage/home/aas6791/scratch/Lake_Problem_DPS/error.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
 #SBATCH --partition=sla-prio
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=48
 #SBATCH --time=4:00:00
@@ -16,4 +16,4 @@
 module load parallel
 
 #job commands below this line
-srun "sh ./runtest5.sh"
+srun -Nodes=2 --ntasks-per-node=1 "sh ./runtest5.sh"
