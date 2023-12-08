46701fb0b42ec806a506a66a35ff08d96efc0c32
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-12-08 13:43:30 -0500

    Update test5.sh

--- test5.sh
+++ test5.sh
@@ -3,12 +3,17 @@
 #SBATCH -o /storage/home/aas6791/scratch/Lake_Problem_DPS/test.%j.out   # Name of the output file
 #SBATCH -e /storage/home/aas6791/scratch/Lake_Problem_DPS/error.%j.err # Name of the error file
 #SBATCH --account=azh5924_b
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=1
 #SBATCH --partition=sla-prio
-#SBATCH --time 0:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=4:00:00
 #SBATCH --mail-user=aas6791@psu.edu
+#SBATCH --export=ALL
+#SBATCH --exclusive=mcs
+#SBATCH --mem=0
 
+module load parallel
 
-#job commands go below this line 
-./test5.py
+#job commands below this line
+srun ./runtest5.sh
