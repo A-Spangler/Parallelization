cc46fe9a0b18832410b6a5965a6d994b838a9930
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2023-12-21 13:58:02 -0700

    add --mpi=pmi2

--- Optimization/DPS/run_DPS_opt_IT.sh
+++ Optimization/DPS/run_DPS_opt_IT.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 #SBATCH -D /storage/home/aas6791/scratch/Lake_Problem_DPS/Optimization/DPS
-#SBATCH -o /storage/home/aas6791/scratch/Lake_Problem_DPS/Optimization/DPS/output/job.%j.out   # Name of the output file (eg. myMPI.oJobID)
+#SBATCH -o /storage/home/aas6791/scratch/Lake_Problem_DPS/Optimization/DPS/output/job.%j.out   
 #SBATCH -e /storage/home/aas6791/scratch/Lake_Problem_DPS/Optimization/DPS/output/error.%j.out
-#SBATCH --nodes=3
-#SBATCH --exclusive
-#SBATCH --ntasks-per-node=20
-#SBATCH --mem-per-cpu=10gb
-#SBATCH --partition=sla-prio           															# Queue name "parallel"
-#SBATCH --account=azh5924_b  																# allocation name
-#SBATCH --time=1:00:00       											 					# Run time (hh:mm:ss) - up to 36 hours
-#SBATCH --mail-user=aas6791@psu.edu             										# address for email notification
-#SBATCH --mail-type=ALL                  												# email at Begin and End of job
+#SBATCH --nodes=3       											 					        # Number of nodes to use
+#SBATCH --exclusive       											 					      # Exclusive use of those nodes
+#SBATCH --ntasks-per-node=20       											 				# Number of tasks each node can perform
+#SBATCH --mem-per-cpu=10gb       											 					# Memory per cpu. specified if changing from preset
+#SBATCH --partition=sla-prio           													# Queue name "parallel"
+#SBATCH --account=azh5924_b  																    # Allocation name
+#SBATCH --time=1:00:00       											 					    # Run time (hh:mm:ss) - up to 36 hours
+#SBATCH --mail-user=aas6791@psu.edu             								# Address for email notification
+#SBATCH --mail-type=ALL                  												# Email at Begin and End of job
 
 module load gcc
-module load openmpi
+module load openmpi/4.1.1
 module load boost/1.77.0
 
 # Your commands go here
 # arguments are <seed> <NFE>
-srun ./LakeDPSparallel 1 100
+srun --mpi=pmi2 -N3 --exclusive --ntasks-per-node=20 ./LakeDPSparallel 1 100
 
-
-#-N3 --exclusive --ntasks-per-node=20
 #doesnt seem like --exclusive or changing -n20 to --ntasks-per-node=20 changed much. (maybe it did change something, but
 #calling ALL these resources in a 20000 times loop was the first error reported and then it aborted before addressing other flags)
