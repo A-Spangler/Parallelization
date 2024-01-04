d885c5de52d04696b63c670d3ed97d9a82241c79
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2024-01-04 09:57:31 -0700

    remove srun flags

--- Optimization/DPS/run_DPS_opt.sh
+++ Optimization/DPS/run_DPS_opt.sh
@@ -7,13 +7,13 @@
 #SBATCH --ntasks=60
 #SBATCH --mem-per-cpu=10gb
 #SBATCH --partition=sla-prio           															# Queue name "parallel"
-#SBATCH --account=azh5924_b  																# allocation name
-#SBATCH --time=00:05:00       											 					# Run time (hh:mm:ss) - up to 36 hours
-#SBATCH --mail-user=aas6791@psu.edu             										# address for email notification
-#SBATCH --mail-type=ALL                  												# email at Begin and End of job
+#SBATCH --account=azh5924_b  														        		# allocation name
+#SBATCH --time=00:05:00       											 				      	# Run time (hh:mm:ss) - up to 36 hours
+#SBATCH --mail-user=aas6791@psu.edu             				 						# address for email notification
+#SBATCH --mail-type=ALL                  									    			# email at Begin and End of job
 
 module load gcc
-module load openmpi/4.1.1
+module load openmpi/4.1.4
 module load boost/1.77.0
 
 # Request resources once before the loop
@@ -22,7 +22,7 @@ srun -N3 --exclusive --ntasks-per-node=20 echo 'Resource allocation for LakeDPSp
 # Run LakeDPSparallel inside the loop
 for i in {1..50}
 do
-  ./LakeDPSparallel 1 100
+  srun ./LakeDPSparallel 1 100
 done
 
 #notes
